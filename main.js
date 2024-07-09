36263ff27a459b669136354f7bbe0a241baea4f5
Desmond Owiso <dowiso@healthstrat.co.ke>
2024-07-09 09:06:05 +0300

    commit please

--- main.js
+++ main.js
@@ -3,3 +3,14 @@ let header = document.querySelector('header');
 window.addEventListener('scroll', () => {
     header.classList.toggle('active', window.scrollY > 0);
 });
+
+
+
+document.addEventListener('DOMContentLoaded', setupNavigation);
+// footer.js
+// main.js
+document.getElementById('menu-icon').addEventListener('click', function() {
+    document.querySelector('.navbar').classList.toggle('active');
+});
+
+
